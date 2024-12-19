9f72cdf2a3361e9478b3d087e421dcf71fc1f4a5
Dragos <45339521+dmarcu1804@users.noreply.github.com>
2024-12-19 17:14:35 +0000

    completed exercise 3.2

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    response.send(`<p> This Page has info for ${persons.length} people </p> <br/> ${new Date()}`)
+})
 app.listen(PORT, () => {
     console.log(`server running on port ${3001}`)
 })
