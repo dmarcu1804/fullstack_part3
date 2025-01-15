91985081d87bf04516b21e401fa8a5743a67af30
Dragos Marcu <45339521+dmarcu1804@users.noreply.github.com>
2025-01-15 17:38:41 +0000

    added db and completed exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3){
+    console.log('give password as argument');
+    console.log(process.argv)
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.2uhb2.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: `${personName}`,
+    number: `${personNumber}`,
+})
+
+if(process.argv.length === 3){
+    Person.find({})
+        .then(result => {
+            console.log("phonebook:")
+            result.forEach(person => {
+                console.log(person.name, person.number)
+            })
+        mongoose.connection.close()
+        })
+}
+else{
+    person.save()
+      .then(result => {
+        console.log(`added ${personName} number ${personNumber} to phonebook`)
+        mongoose.connection.close()
+      })
+}
