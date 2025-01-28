e78becbef3077d654ce1e9fc915c0ae93c2c1bef
Dragos <45339521+dmarcu1804@users.noreply.github.com>
2025-01-28 16:58:54 +0000

    added validation backend

--- index.js
+++ index.js
@@ -81,8 +81,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
-  const id = Math.random() * 100;
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -98,7 +97,8 @@ app.post("/api/persons", (request, response) => {
 
   personObject.save().then((savedPerson) => {
     response.json(savedPerson);
-  });
+  })
+  .catch(error => next(error))
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
@@ -111,7 +111,7 @@ app.put("/api/persons/:id", (request, response, next) => {
   };
 
   if (personNameExists) {
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators:true, context:'query' })
       .then((updatedPerson) => {
         response.json(updatedPerson);
       })
@@ -139,6 +139,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if(error.name === "ValidationError"){
+    return response.status(400).json({error: error.message})
   }
 
   next(error);
